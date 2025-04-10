f15a0c045314390761abfbb9845a7cbe35fd3047
Pedro Carlos - Cristal Engenharia <pedro.arantes@cristal.eng.br>
2025-04-10 17:46:52 -0300

    Arquivo inicial

---
+++ menu-struct.c
@@ -0,0 +1,12 @@
+//definição da estrutura de dados que os restaurantes precisam para cadastrar seus pratos
+#include <stdio.h>
+
+int main () {
+
+    struct menu_restaurante
+    {
+        /* data */
+    };
+    
+
+}
