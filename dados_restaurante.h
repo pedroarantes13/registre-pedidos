9097170778ec0a0671239d4713b222a299090872
pedroarantes13 <p3ddro.carlos@gmail.com>
2025-04-11 01:22:44 -0300

    Criadas funções e bibliotecas de escrita de dados

---
+++ dados_restaurante.h
@@ -0,0 +1,7 @@
+#ifndef DADOS_RESTAURANTE_H
+#define DADOS_RESTAURANTE_H
+
+void escrever_dados(char[], int, float, int);
+void ler_dados();
+
+#endif
---
+++ main.c
@@ -0,0 +1,14 @@
+#include "dados_restaurante.h"
+
+int main () {
+
+//exemplos de dados recebidos do usuario(restaurante)	
+
+	char nome[] = "Lagosta";
+	int tipo = 2;
+	float valor = 500;
+	int tempo_preparo = 120;
+
+	escrever_dados(nome, tipo, valor, tempo_preparo);
+
+}
