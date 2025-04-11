f8bb4dbfebda5dc265862871b388c4f04bddb72d
pedroarantes13 <p3ddro.carlos@gmail.com>
2025-04-10 23:54:52 -0300

    Atualização

--- menu-struct.c
+++ menu-struct.c
@@ -1,13 +1,13 @@
 //definição da estrutura de dados que os restaurantes precisam para cadastrar seus pratos
 #include <stdio.h>
 
+struct menu_restaurante {
+	char nome[100];
+	int tipo;
+	float valor;
+	float tempo_preparo;
+};
+
 int main () {
 
-    struct menu_restaurante
-    {
-        char nome[100];
-        int tipo;
-        float preço;
-        float tempo_preparo;
-    };
 }
